5e799e9a64dee2d23487be576170c455f202426c
NureKononykhinaTaisiia <taisiia.kononykhina@nure.ua>
2025-04-23 19:12:37 +0200

    Init

---
+++ Lab2/pzpi-22-2-kononykhina-taisiia-lab2/routes/products.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const router = express.Router();
+const productController = require("../controller/products");
+const multer = require("multer");
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/uploads/products");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "_" + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+router.use((req, res, next) => {
+  console.log("=== [Router level] Incoming request ===");
+  console.log("Method:", req.method);
+  console.log("URL:", req.originalUrl);
+  console.log("Headers:", req.headers);
+  console.log("Query:", req.query);
+  console.log("Params:", req.params);
+  next(); // обязательно
+});
+router.get("/all-product", productController.getAllProduct);
+router.post("/product-by-category", productController.getProductByCategory);
+router.post("/product-by-price", productController.getProductByPrice);
+router.post("/wish-product", productController.getWishProduct);
+router.post("/cart-product", productController.getCartProduct);
+
+router.post("/add-product", upload.any(), productController.postAddProduct);
+router.post("/edit-product", upload.any(), productController.postEditProduct);
+router.post("/delete-product", productController.getDeleteProduct);
+router.post("/single-product", productController.getSingleProduct);
+
+router.post("/add-review", productController.postAddReview);
+router.post("/delete-review", productController.deleteReview);
+
+module.exports = router;
